68e3fea0e95359b8ca4b71b7855564ff417c0f35
Aditya shah <adiityaashah@gmail.com>
2026-09-23 22:46:10 +0530

    Time: 3 ms (93.87%) | Memory: 44.2 MB (81.69%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isAnagram(String s, String t) 
+    {
+        if(s.length()!=t.length())
+        {
+            return false;
+        }
+        else
+        {
+            int count[]=new int[26];
+            for(int i=0;i<s.length();i++)
+            {
+                count[s.charAt(i)-'a']++;
+            }
+            for(int j=0;j<t.length();j++)
+            {
+                  count[t.charAt(j)-'a']--;
+            }
+
+            for(int i=0;i<count.length;i++)
+            {
+                if(count[i]!=0)
+                {
+                    return false;
+                }
+            }
+             return true; 
+
+        }
+    }
+}
